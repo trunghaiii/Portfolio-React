5f75d95f4bf82ace23fbe57668fa8268ea03c82d
trunghaiii <haitran022001@gmail.com>
2023-11-05 18:32:53 -0500

    structure project layout (Part 2)

---
+++ src/components/Preloader.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+
+const Preloader = () => {
+    return (
+        <>
+            {/* <div className="arlo_tm_preloader">
+  <div className="spinner_wrap">
+    <div className="spinner"></div>
+  </div>
+</div> */}
+        </>
+    )
+}
+
+export default Preloader
