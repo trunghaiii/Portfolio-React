b528ebd057a06fa042c4f2c34f863d4a7a1001da
trunghaiii <haitran022001@gmail.com>
2023-11-05 20:39:20 -0500

    config scss files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Portfolio from './Screens/Portfolio'
+import "./styles/plugins.scss"
+import "./styles/style.scss"
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
