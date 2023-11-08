6fd7d01c7827e9756066feef0ed4ef1c2af470f5
trunghaiii <haitran022001@gmail.com>
2023-11-07 20:20:04 -0500

    Working on Skill component

---
+++ src/components/content/Skill.tsx
@@ -0,0 +1,126 @@
+
+
+
+const Skill = () => {
+    return (
+        <div className="arlo_tm_section" id="skills">
+            <div className="arlo_tm_skills_wrap">
+                <div className="container">
+                    <div className="inner_wrap">
+                        <div className="leftbox">
+                            <div className="arlo_tm_mini_title_holder">
+                                <h3>Programming Languages</h3>
+                            </div>
+
+                        </div>
+                        <div className="rightbox">
+                            <div className="progress_bar_wrap_total">
+                                <div className="arlo_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
+                                    <div className="arlo_tm_progress" data-value="95" data-color="#000">
+                                        <span><span className="label">C, Java, Python, JavaScript/TypeScript</span></span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="arlo_tm_skills_wrap">
+                <div className="container">
+                    <div className="inner_wrap">
+                        <div className="leftbox">
+                            <div className="arlo_tm_mini_title_holder">
+                                <h3>Front-End Stack</h3>
+                            </div>
+
+                        </div>
+                        <div className="rightbox">
+                            <div className="progress_bar_wrap_total">
+                                <div className="arlo_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
+                                    <div className="arlo_tm_progress" data-value="95" data-color="#000">
+                                        <span><span className="label">HTML, CSS/SCSS, Redux, React JS, Bootstrap, Ant Design</span></span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="arlo_tm_skills_wrap">
+                <div className="container">
+                    <div className="inner_wrap">
+                        <div className="leftbox">
+                            <div className="arlo_tm_mini_title_holder">
+                                <h3>Back-End Stack</h3>
+                            </div>
+
+                        </div>
+                        <div className="rightbox">
+                            <div className="progress_bar_wrap_total">
+                                <div className="arlo_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
+                                    <div className="arlo_tm_progress" data-value="95" data-color="#000">
+                                        <span><span className="label">Node JS Platform, Express JS, Nest JS, SQL, NoSQL, REST API</span></span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="arlo_tm_skills_wrap">
+                <div className="container">
+                    <div className="inner_wrap">
+                        <div className="leftbox">
+                            <div className="arlo_tm_mini_title_holder">
+                                <h3>DBMS</h3>
+                            </div>
+
+                        </div>
+                        <div className="rightbox">
+                            <div className="progress_bar_wrap_total">
+                                <div className="arlo_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
+                                    <div className="arlo_tm_progress" data-value="95" data-color="#000">
+                                        <span><span className="label">PostgreSQL, MongoDB</span></span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="arlo_tm_skills_wrap">
+                <div className="container">
+                    <div className="inner_wrap">
+                        <div className="leftbox">
+                            <div className="arlo_tm_mini_title_holder">
+                                <h3>Tools</h3>
+                            </div>
+
+                        </div>
+                        <div className="rightbox">
+                            <div className="progress_bar_wrap_total">
+                                <div className="arlo_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
+                                    <div className="arlo_tm_progress" data-value="95" data-color="#000">
+                                        <span><span className="label">Git/GitHub, Docker, Postman, …</span></span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Skill
