8eb0e2f5de09113c13083f660b0152c627e9794e
trunghaiii <haitran022001@gmail.com>
2023-11-09 16:44:11 -0500

    Create Project component

---
+++ src/components/content/Project.tsx
@@ -0,0 +1,100 @@
+
+
+const Project = () => {
+    return (
+        <div className="arlo_tm_section" id="services">
+            <div className="arlo_tm_services_wrap">
+                <div className="container">
+                    <div className="arlo_tm_title_holder">
+                        <h3>Personal Projects</h3>
+                        <span>Meet my amazing projects</span>
+                    </div>
+                    <div className="list_wrap">
+                        <ul>
+                            <li>
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img className="svg" src="img/svg/camera-diaphragm.svg" alt="camera-diaphragm" />
+                                    </div>
+                                    <div className="title_service">
+                                        <h3>Photography</h3>
+                                    </div>
+                                    <div className="text">
+                                        <p>Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img className="svg" src="img/svg/new-tab.svg" alt="new-tab" />
+                                    </div>
+                                    <div className="title_service">
+                                        <h3>Web Design</h3>
+                                    </div>
+                                    <div className="text">
+                                        <p>Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img className="svg" src="img/svg/layers.svg" alt="layers" />
+                                    </div>
+                                    <div className="title_service">
+                                        <h3>Branding</h3>
+                                    </div>
+                                    <div className="text">
+                                        <p>Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img className="svg" src="img/svg/share.svg" alt="share" />
+                                    </div>
+                                    <div className="title_service">
+                                        <h3>Social Media</h3>
+                                    </div>
+                                    <div className="text">
+                                        <p>Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img className="svg" src="img/svg/adobe-illustrator.svg" alt="adobe-illustrator" />
+                                    </div>
+                                    <div className="title_service">
+                                        <h3>Illustrator</h3>
+                                    </div>
+                                    <div className="text">
+                                        <p>Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="inner">
+                                    <div className="icon">
+                                        <img className="svg" src="img/svg/seo-performance-marketing-graphic.svg" alt="seo-performance-marketing-graphic" />
+                                    </div>
+                                    <div className="title_service">
+                                        <h3>Marketing</h3>
+                                    </div>
+                                    <div className="text">
+                                        <p>Web design is a similar process of creation, with the intention of presenting the content on electronic pages ...</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project
